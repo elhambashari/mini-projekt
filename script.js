4f0c3847da3af8cfdc2044f4d7af8c5f59402856
Elham Bashari <elhambashari84@gmail.com>
2025-01-11 13:37:59 +0100

    script.js created

---
+++ script.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('h1')
+const firstP = document.querySelector('p')
+
+h1.addEventListener('click' ,()=>{
+firstP.innerHTML ='A pleasant place for you and your family.'
+firstP.style.color ='red'
+firstP.style.fontSize = '20 px'
+})
+
+console.log("elham")
