445de8058e1893382e15cac4aaf26d3bd7b521ec
Elham Bashari <elhambashari84@gmail.com>
2025-01-11 13:55:58 +0100

    js cod added

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const h1 = document.querySelector('h1')
 const firstP = document.querySelector('p')
 
-h1.addEventListener('click' ,()=>{
+h1.addEventListener('click',()=>{
 firstP.innerHTML ='A pleasant place for you and your family.'
-firstP.style.color ='red'
-firstP.style.fontSize = '20 px'
+firstP.style.color ='violet'
+firstP.style.fontSize = '2rem'
+h1.style.display ='none'
 })
 
-console.log("elham")
